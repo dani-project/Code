a51dc71656a85dfd083102f959e7cdc591877989
YeMyatThwin <yemyat.23@ichat.sp.edu.sg>
2024-07-23 10:45:21 +0800

    CRUD set 1 from GROUP 03

--- server/src/routes/mainRoute.js
+++ server/src/routes/mainRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const siteRoute = require("./siteRoute");
 const featureRoute = require("./featureRoute");
 const pfuncRoute = require("./pfuncRoute");
 const paymentRoute = require("./paymentRoute");
@@ -8,6 +9,8 @@ const functionalitiesRoute = require("./functionalitiesRoute");
 //==============================================================//
 //                    Main Routes                        //
 //==============================================================//
+// Feature Routes
+router.use("/sites", siteRoute);
 // Feature Routes
 router.use("/feature", featureRoute);
 // Parent Functionalities Routes
